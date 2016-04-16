bc22f27ee6e10cf9ebc447e21ab8d85291f92aa6
Sturla Freyr <sturlaf7@gmail.com>
2016-04-16 22:11:14 +0000

    byrja að flesha út modelið

---
+++ app/src/main/java/com/example/freydis/drinklink/model/Tab.java
@@ -0,0 +1,35 @@
+package com.example.freydis.drinklink.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Stulli on 16/04/2016.
+ */
+public class Tab {
+    public ArrayList<Transaction> transactions = new ArrayList<~>();
+    public User owner = null;
+
+    public Tab(){
+
+    }
+
+    public void updateTab(List<Transaction>){
+
+    }
+
+    public void removeTransaction(Transaction){
+
+    }
+
+    public Transaction getMostRecentTransaction(){
+
+    }
+
+    public Transaction getSpecificTransaction(Transaction){
+
+    }
+    public void clear(){
+
+    }
+}
---
+++ app/src/main/java/com/example/freydis/drinklink/model/Transaction.java
@@ -0,0 +1,69 @@
+package com.example.freydis.drinklink.model;
+
+import java.util.List;
+import java.util.Date;
+
+
+/**
+ * Created by Stulli on 16/04/2016.
+ */
+public class Transaction {
+    public List<Drink> drinks;
+    public User userTo;
+    public User userFrom;
+    public String location;
+    public String note;
+    public Date timestamp;
+
+    public Transaction(){
+
+    }
+
+    public List<Drink> getDrinks(){
+        return this.drinks;
+    }
+
+    public void setDrinks(List<Drink> d){
+        this.drinks = d;
+    }
+
+    public User getUserTo(){
+        return this.userTo;
+    }
+
+    public void setUserTo(User u){
+        this.userTo = u;
+    }
+
+    public User getUserFrom(){
+        return this.userFrom;
+    }
+
+    public void setUserFrom(User u){
+        this.userFrom = u;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(String l){
+        this.location = l;
+    }
+
+    public String getNote(){
+        return this.note;
+    }
+
+    public void setNote(String n){
+        this.note = n;
+    }
+
+    public Date getTimestamp(){
+        return this.timestamp;
+    }
+
+    public void setTimestamp(String t){
+        this.timestamp = t;
+    }
+}
